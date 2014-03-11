81de628c46faed3ccc035241bb8be622f79b8c0a
Jérémy Carrat <skeletongamer@sylaturia.net>
2014-03-11 17:28:45 +0100

    Correction du Storyboard

--- TestParse/PhotosAlbumViewController.m
+++ TestParse/PhotosAlbumViewController.m
@@ -460,9 +460,7 @@
                     
                     if (self.datasourceAutomatic.count == 0) {
                         [self.selectButton removeFromSuperview];
-                        self.headerConstraint.constant = 0;
-                        
-                        [self.collectionView setContentInset:UIEdgeInsetsMake(64, 0, 0, 0)];
+                        self.headerConstraint.constant = 64;
                     } else {
                         self.navigationItem.rightBarButtonItem.enabled = YES;
                     }
